afe24454b86382902fc05e1997dfa5a459f65e1c
Asim Abdalla <asimabdalla99@gmail.com>
2025-02-01 11:19:28 +0000

    refactor: update model names and abbreviations in watermark generation script

--- scripts/generate_with_watermark.sh
+++ scripts/generate_with_watermark.sh
@@ -18,16 +18,14 @@ MODEL_NAMES=(
     "meta-llama/Llama-2-7b-hf"
     "baichuan-inc/Baichuan2-7B-Base"
     "baichuan-inc/Baichuan-7B"
-    "google/gemma-2b"
-    # "mistralai/Mistral-7B-v0.1"
+    "mistralai/Mistral-7B-v0.1"
 )
 
 MODEL_ABBRS=(
     "llama2-7b"
     "baichuan2-7b"
     "baichuan-7b"
-    "gemma-2b"
-    # "mistral-7b"
+    "mistral-7b"
 )
 
 WATERMARK_METHODS=(
