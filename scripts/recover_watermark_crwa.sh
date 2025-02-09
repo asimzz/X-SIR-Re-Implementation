1bd1e1ea23e3fa2d330803deaf0a377a4d5d18be
Asim Abdalla <asimabdalla99@gmail.com>
2025-02-09 14:08:42 +0000

    feat: add recover_watermark_crwa.sh script for watermark recovery process

---
+++ scripts/recover_watermark_crwa.sh
@@ -0,0 +1,69 @@
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error when substituting.
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+WORK_DIR=$SCRIPT_DIR/..
+DATA_DIR=$WORK_DIR/data
+GEN_DIR=$WORK_DIR/gen
+ATTACK_DIR=$WORK_DIR/attack
+
+# Parameters for SIR/X-SIR
+MAPPING_DIR=$WORK_DIR/data/mapping
+TRANSFORM_MODEL=$WORK_DIR/data/model/transform_model_x-sbert_10K.pth
+EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
+
+BATCH_SIZE=4
+
+MODEL_NAMES=(
+    "meta-llama/Llama-2-7b-hf"
+    # "baichuan-inc/Baichuan2-7B-Base"
+    # "baichuan-inc/Baichuan-7B"
+)
+
+MODEL_ABBRS=(
+    "llama2-7b"
+    # "baichuan2-7b"
+    # "baichuan-7b"
+)
+
+WATERMARK_METHODS=(
+    "kgw"
+)
+
+ORG_LANG="en"
+PVT_LANG="zh"
+
+if [ ${#MODEL_NAMES[@]} -ne ${#MODEL_ABBRS[@]} ]; then
+    echo "Length of MODEL_NAMES and MODEL_ABBRS should be the same"
+    exit 1
+fi
+
+for i in "${!MODEL_NAMES[@]}"; do
+    MODEL_NAME=${MODEL_NAMES[$i]}
+    MODEL_ABBR=${MODEL_ABBRS[$i]}
+
+    for WATERMARK_METHOD in "${WATERMARK_METHODS[@]}"; do
+        echo "Generating with watermark for $MODEL_NAME using $WATERMARK_METHOD"
+
+        if [ $WATERMARK_METHOD == "kgw" ]; then
+            WATERMARK_METHOD_FLAG="--watermark_method kgw"
+        else
+            echo "Unknown watermark method: $WATERMARK_METHOD"
+            exit 1
+        fi
+
+        python3 $ATTACK_DIR/translate.py \
+                --input_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$ORG_LANG-crwa.mod.jsonl \
+                --output_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$PVT_LANG-crwa-back.mod.jsonl \
+                --model sophosympatheia/rogue-rose-103b-v0.2:free \
+                --src_lang $ORG_LANG \
+                --tgt_lang $PVT_LANG \
+                --translate_part response
+
+        python3 $WORK_DIR/detect.py \
+            --base_model $MODEL_NAME \
+            --detect_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$PVT_LANG-crwa-back.mod.jsonl \
+            --output_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$PVT_LANG-crwa-back.mod.z_score.jsonl \
+            $WATERMARK_METHOD_FLAG
+    done
+done
