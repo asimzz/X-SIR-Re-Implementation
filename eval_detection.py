862d536eeddea8ab5bf54323635e4016f3985858
Asim Abdalla <asimabdalla99@gmail.com>
2025-02-10 14:34:32 +0000

    fix: increase the limit of hm_list to 100 in eval_detection.py for improved data handling

--- eval_detection.py
+++ eval_detection.py
@@ -28,7 +28,7 @@ def f1_at_fpr(y_true, y_scores, fpr_target):
     return f1
 
 def main(args):
-    hm_list = read_jsonl(args.hm_zscore)[:98]
+    hm_list = read_jsonl(args.hm_zscore)[:100]
     wm_list = read_jsonl(args.wm_zscore)
     
     if len(hm_list) != len(wm_list):
