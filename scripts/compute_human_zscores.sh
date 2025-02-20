ceb703fdc41389d30738e33cf43226c8c620f738
Asim Abdalla <asimabdalla99@gmail.com>
2025-02-20 15:41:00 +0000

    add: create compute_human_zscores.sh for watermark generation with models

---
+++ scripts/compute_human_zscores.sh
@@ -0,0 +1,70 @@
+set -e # Exit immediately if a command exits with a non-zero status.
+set -u # Treat unset variables as an error when substituting.
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+WORK_DIR=$SCRIPT_DIR/..
+DATA_DIR=$WORK_DIR/data
+GEN_DIR=$WORK_DIR/gen
+ATTACK_DIR=$WORK_DIR/attack
+
+# Parameters for SIR/X-SIR
+MAPPING_DIR=$WORK_DIR/data/mapping
+TRANSFORM_MODEL=$WORK_DIR/data/model/transform_model_x-sbert_10K.pth
+EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
+
+BATCH_SIZE=8
+
+MODEL_NAMES=(
+    "bigscience/bloom-7b1"
+    "CohereForAI/aya-23-8B"
+    "meta-llama/Llama-3.1-8B-Instruct"
+    "meta-llama/Llama-2-7b-hf"
+    "baichuan-inc/Baichuan2-7B-Base"
+    "baichuan-inc/Baichuan-7B"
+
+)
+
+MODEL_ABBRS=(
+    "bloom-7b1"
+    "aya-23-8B"
+    "llama-3.1-8B"
+    "llama2-7b"
+    "baichuan2-7b"
+    "baichuan-7b"
+)
+
+WATERMARK_METHODS=(
+    "kgw"
+    "xsir"
+)
+
+
+if [ ${#MODEL_NAMES[@]} -ne ${#MODEL_ABBRS[@]} ]; then
+    echo "Length of MODEL_NAMES and MODEL_ABBRS should be the same"
+    exit 1
+fi
+
+for i in "${!MODEL_NAMES[@]}"; do
+    MODEL_NAME=${MODEL_NAMES[$i]}
+    MODEL_ABBR=${MODEL_ABBRS[$i]}
+
+    for WATERMARK_METHOD in "${WATERMARK_METHODS[@]}"; do
+        echo "Generating with watermark for $MODEL_NAME using $WATERMARK_METHOD"
+
+        if [ $WATERMARK_METHOD == "kgw" ]; then
+            WATERMARK_METHOD_FLAG="--watermark_method kgw"
+        elif [ $WATERMARK_METHOD == "sir" ] || [ $WATERMARK_METHOD == "xsir" ]; then
+            WATERMARK_METHOD_FLAG="--watermark_method xsir  --transform_model $TRANSFORM_MODEL --embedding_model $EMBEDDING_MODEL --mapping_file $MAPPING_DIR/$WATERMARK_METHOD/300_mapping_$MODEL_ABBR.json"
+        else
+            echo "Unknown watermark method: $WATERMARK_METHOD"
+            exit 1
+        fi
+
+        python3 $WORK_DIR/detect.py \
+                --base_model $MODEL_NAME \
+                --detect_file $DATA_DIR/dataset/mc4/mc4.en-100.jsonl \
+                --output_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
+                $WATERMARK_METHOD_FLAG
+        done
+    done
+done
