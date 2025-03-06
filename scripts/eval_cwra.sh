c6cd035212e3e09359a484c953d119d68359fd72
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-06 23:53:56 +0000

    fix: update watermark type to 'window' and add window size parameter in detect.py; modify eval_cwra.sh for improved language handling

--- scripts/eval_cwra.sh
+++ scripts/eval_cwra.sh
@@ -10,8 +10,7 @@ ATTACK_DIR=$WORK_DIR/attack
 MODEL_NAMES=(
     "bigscience/bloom-7b1"
     "CohereForAI/aya-23-8B"
-    "meta-llama/Llama-2-7b-hf"
-    # "CohereForAI/aya-101"
+    # # "meta-llama/Llama-2-7b-hf"
     # "baichuan-inc/Baichuan2-7B-Base"
     # "baichuan-inc/Baichuan-7B"
 )
@@ -19,8 +18,7 @@ MODEL_NAMES=(
 MODEL_ABBRS=(
     "bloom-7b1"
     "aya-23-8B"
-    "llama2-7b"
-    # "aya-101"
+    # # "llama2-7b"
     # "baichuan2-7b"
     # "baichuan-7b"
 )
@@ -31,7 +29,14 @@ WATERMARK_METHODS=(
 )
 
 ORG_LANG="en"
-PVT_LANG="zh"
+PVT_LANGS=(
+    "ar"
+    "tr"
+    "de"
+    "fr"
+    # "zh"
+    # "ja"
+)
 
 
 if [ ${#MODEL_NAMES[@]} -ne ${#MODEL_ABBRS[@]} ]; then
@@ -44,32 +49,33 @@ for i in "${!MODEL_NAMES[@]}"; do
     MODEL_ABBR=${MODEL_ABBRS[$i]}
 
     for WATERMARK_METHOD in "${WATERMARK_METHODS[@]}"; do
+        for PVT_LANG in "${PVT_LANGS[@]}"; do
+            echo "$MODEL_NAME $WATERMARK_METHOD Without CWRA Attack ($ORG_LANG -> $PVT_LANG)"
+            python3 $WORK_DIR/eval_detection.py \
+                --hm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
+                --wm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$ORG_LANG-$PVT_LANG-cwra.mod.z_score.jsonl \
+                --roc_curve $FIGURE_DIR/$MODEL_ABBR/$WATERMARK_METHOD/no_cwra-$PVT_LANG.txt
 
-        echo "$MODEL_NAME $WATERMARK_METHOD Without CWRA Attack"
-        python3 $WORK_DIR/eval_detection.py \
-            --hm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
-            --wm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$ORG_LANG-$PVT_LANG-crwa.mod.z_score.jsonl \
-            --roc_curve $FIGURE_DIR/$MODEL_ABBR/$WATERMARK_METHOD/no_cwra-$PVT_LANG.txt
+            echo "======================================="
 
-        echo "======================================="
+            echo "$MODEL_NAME $WATERMARK_METHOD With CWRA Attack ($PVT_LANG -> $ORG_LANG)"
+            python3 $WORK_DIR/eval_detection.py \
+                --hm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
+                --wm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$PVT_LANG-$ORG_LANG-cwra.mod.z_score.jsonl \
+                --roc_curve $FIGURE_DIR/$MODEL_ABBR/$WATERMARK_METHOD/cwra_attack-$PVT_LANG.txt
 
-        echo "$MODEL_NAME $WATERMARK_METHOD With CWRA Attack"
-        python3 $WORK_DIR/eval_detection.py \
-            --hm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
-            --wm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$PVT_LANG-$ORG_LANG-crwa.mod.z_score.jsonl \
-            --roc_curve $FIGURE_DIR/$MODEL_ABBR/$WATERMARK_METHOD/cwra_attack-$PVT_LANG.txt
+            echo "======================================="
 
-        echo "======================================="
+            if [ $WATERMARK_METHOD == "xsir" ]; then
+                continue
+            fi
+            echo "$MODEL_NAME $WATERMARK_METHOD With CWRA Attack (Back Translation) ($ORG_LANG -> $PVT_LANG)"
+            python3 $WORK_DIR/eval_detection.py \
+                --hm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
+                --wm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$ORG_LANG-$PVT_LANG-cwra-back.mod.z_score.jsonl \
+                --roc_curve $FIGURE_DIR/$MODEL_ABBR/$WATERMARK_METHOD/cwra_attack_back-$PVT_LANG.txt
 
-        if [ $WATERMARK_METHOD == "xsir" ]; then
-            continue
-        fi
-        echo "$MODEL_NAME $WATERMARK_METHOD With CWRA Attack (Back Translation)"
-        python3 $WORK_DIR/eval_detection.py \
-            --hm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
-            --wm_zscore $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.$ORG_LANG-$PVT_LANG-crwa-back.mod.z_score.jsonl \
-            --roc_curve $FIGURE_DIR/$MODEL_ABBR/$WATERMARK_METHOD/cwra_attack_back-$PVT_LANG.txt
-
-        echo "======================================="
+            echo "======================================="
+        done
     done
 done
