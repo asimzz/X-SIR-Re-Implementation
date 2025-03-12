2e41c86a82db174f8802663779d895f50cf9aa10
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-12 03:07:07 +0000

    fix: increase batch size from 8 to 16 in generate_with_watermark_translate.sh; comment out unused model entries

--- scripts/generate_with_watermark_translate.sh
+++ scripts/generate_with_watermark_translate.sh
@@ -12,7 +12,7 @@ MAPPING_DIR=$WORK_DIR/data/mapping
 TRANSFORM_MODEL=$WORK_DIR/data/model/transform_model_x-sbert_10K.pth
 EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
 
-BATCH_SIZE=8
+BATCH_SIZE=16
 
 MODEL_NAMES=(
     "bigscience/bloom-7b1"
@@ -20,8 +20,8 @@ MODEL_NAMES=(
     "meta-llama/Llama-3.1-8B-Instruct"
     "meta-llama/Llama-3.2-1B"
     "meta-llama/Llama-2-7b-hf"
-    "google/mt5-base"
-    "facebook/m2m100_418M"
+    # "google/mt5-base"
+    # "facebook/m2m100_418M"
     "baichuan-inc/Baichuan2-7B-Base"
     "baichuan-inc/Baichuan-7B"
 )
@@ -32,8 +32,8 @@ MODEL_ABBRS=(
     "llama-3.1-8B"
     "llama-3.2-1B"
     "llama2-7b"
-    "mt5-base"
-    "m2m100_418M"
+    # "mt5-base"
+    # "m2m100_418M"
     "baichuan2-7b"
     "baichuan-7b"
 )
