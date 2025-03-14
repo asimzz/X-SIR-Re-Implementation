a817b8550da7b3ff76110b5cbc3b03e8f0b3b8d5
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-14 15:38:04 +0000

    update output file naming in compute_hum_zscore.sh to reflect surrogate z-score

--- scripts/compute_hum_zscore.sh
+++ scripts/compute_hum_zscore.sh
@@ -59,9 +59,8 @@ for i in "${!MODEL_NAMES[@]}"; do
         python3 $WORK_DIR/detect.py \
             --base_model $MODEL_NAME \
             --detect_file $DATA_DIR/dataset/mc4/mc4.en.jsonl \
-            --output_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
+            --output_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.surrogate.z_score.jsonl \
             $WATERMARK_METHOD_FLAG
 
-       
     done
 done
