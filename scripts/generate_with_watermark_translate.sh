93b9d975a607e1678d3bb84bc408d39460f66ec2
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-18 22:14:40 +0000

    fix: update model entries in generate_with_watermark_translate.sh; enable Llama-2 and Baichuan models while commenting out others

--- scripts/generate_with_watermark_translate.sh
+++ scripts/generate_with_watermark_translate.sh
@@ -19,11 +19,11 @@ MODEL_NAMES=(
     # "CohereForAI/aya-23-8B"
     # "meta-llama/Llama-3.1-8B-Instruct"
     # "meta-llama/Llama-3.2-1B"
-    # "meta-llama/Llama-2-7b-hf"
-    "google/mt5-base"
+    "meta-llama/Llama-2-7b-hf"
+    # "google/mt5-base"
     # "facebook/m2m100_418M"
-    # "baichuan-inc/Baichuan2-7B-Base"
-    # "baichuan-inc/Baichuan-7B"
+    "baichuan-inc/Baichuan2-7B-Base"
+    "baichuan-inc/Baichuan-7B"
 )
 
 MODEL_ABBRS=(
@@ -31,11 +31,11 @@ MODEL_ABBRS=(
     # "aya-23-8B"
     # "llama-3.1-8B"
     # "llama-3.2-1B"
-    # "llama2-7b"
-    "mt5-base"
+    "llama2-7b"
+    # "mt5-base"
     # "m2m100_418M"
-    # "baichuan2-7b"
-    # "baichuan-7b"
+    "baichuan2-7b"
+    "baichuan-7b"
 )
 
 WATERMARK_METHODS=(
