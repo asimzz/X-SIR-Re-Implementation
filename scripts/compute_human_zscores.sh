ed3b91a51cc5433c5bf461b0a0adfe919b154e35
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-24 04:57:29 +0000

    fix: comment out unused model names and abbreviations in compute_human_zscores.sh

--- scripts/compute_human_zscores.sh
+++ scripts/compute_human_zscores.sh
@@ -15,22 +15,22 @@ EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
 BATCH_SIZE=8
 
 MODEL_NAMES=(
-    "bigscience/bloom-7b1"
-    "CohereForAI/aya-23-8B"
-    "meta-llama/Llama-3.1-8B-Instruct"
-    "meta-llama/Llama-2-7b-hf"
+    # "bigscience/bloom-7b1"
+    # "CohereForAI/aya-23-8B"
+    # "meta-llama/Llama-3.1-8B-Instruct"
+    # "meta-llama/Llama-2-7b-hf"
     "baichuan-inc/Baichuan2-7B-Base"
-    "baichuan-inc/Baichuan-7B"
+    # "baichuan-inc/Baichuan-7B"
 
 )
 
 MODEL_ABBRS=(
-    "bloom-7b1"
-    "aya-23-8B"
-    "llama-3.1-8B"
-    "llama2-7b"
+    # "bloom-7b1"
+    # "aya-23-8B"
+    # "llama-3.1-8B"
+    # "llama2-7b"
     "baichuan2-7b"
-    "baichuan-7b"
+    # "baichuan-7b"
 )
 
 WATERMARK_METHODS=(
@@ -62,7 +62,7 @@ for i in "${!MODEL_NAMES[@]}"; do
 
         python3 $WORK_DIR/detect.py \
                 --base_model $MODEL_NAME \
-                --detect_file $DATA_DIR/dataset/mc4/mc4.en.jsonl \
+                --detect_file $DATA_DIR/dataset/mc4/mc4.en-100.jsonl \
                 --output_file $GEN_DIR/$MODEL_ABBR/$WATERMARK_METHOD/mc4.en.hum.z_score.jsonl \
                 $WATERMARK_METHOD_FLAG
         done
