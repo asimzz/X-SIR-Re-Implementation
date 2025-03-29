3e199d6c276e3d79af1c3bfda31139d5f388b021
Asim Abdalla <asimabdalla99@gmail.com>
2025-03-29 00:20:34 +0000

    update: remove deprecated model name from compute_human_zscores script

--- scripts/compute_human_zscores.sh
+++ scripts/compute_human_zscores.sh
@@ -15,7 +15,6 @@ EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
 MODEL_NAMES=(
     "bigscience/bloom-7b1"
     "CohereForAI/aya-23-8B"
-    "meta-llama/Llama-3.1-8B-Instruct"
     "meta-llama/Llama-3.2-1B"
     "facebook/xglm-564M"
     "baichuan-inc/Baichuan2-7B-Base"
@@ -25,14 +24,13 @@ MODEL_NAMES=(
 MODEL_ABBRS=(
     "bloom-7b1"
     "aya-23-8B"
-    "llama-3.1-8B"
     "llama-3.2-1B"
     "xglm-564M"
     "baichuan2-7b"
 )
 
 WATERMARK_METHODS=(
-    # "kgw"
+    "kgw"
     "xsir"
 )
 
