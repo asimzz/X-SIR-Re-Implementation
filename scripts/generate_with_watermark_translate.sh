82c3587e822eed73174fe192de42efbc4a1994de
Asim Abdalla <asimabdalla99@gmail.com>
2025-04-16 12:41:42 +0000

    update translation script code

--- scripts/generate_with_watermark_translate.sh
+++ scripts/generate_with_watermark_translate.sh
@@ -17,17 +17,17 @@ BATCH_SIZE=8
 MODEL_NAMES=(
     "meta-llama/Llama-3.2-1B"
     "baichuan-inc/Baichuan2-7B-Base"
+    "bigscience/bloom-7b1"
     "CohereForAI/aya-23-8B"
     "facebook/xglm-564M"
-    "bigscience/bloom-7b1"
 )
 
 MODEL_ABBRS=(
     "llama-3.2-1B"
     "baichuan2-7b"
+    "bloom-7b1"
     "aya-23-8B"
     "xglm-564M"
-    "bloom-7b1"
 )
 
 WATERMARK_METHODS=(
