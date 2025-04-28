6654cee1070e05c0ec4b1bb572140ae4722c850b
Asim Abdalla <asimabdalla99@gmail.com>
2025-04-28 13:55:53 +0200

    update: increase batch size to 32 and add additional target languages for translation

--- scripts/generate_with_watermark_translate.sh
+++ scripts/generate_with_watermark_translate.sh
@@ -12,7 +12,7 @@ MAPPING_DIR=$WORK_DIR/data/mapping
 TRANSFORM_MODEL=$WORK_DIR/data/model/transform_model_x-sbert.pth
 EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
 
-BATCH_SIZE=8
+BATCH_SIZE=32
 
 MODEL_NAMES=(
     "meta-llama/Llama-3.2-1B"
@@ -42,6 +42,10 @@ TGT_LANGS=(
     "fr"
     "zh"
     "ja"
+    "ar"
+    "tr"
+    "sw"
+    "am"
 )
 
 if [ ${#MODEL_NAMES[@]} -ne ${#MODEL_ABBRS[@]} ]; then
