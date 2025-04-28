e1b712285ea0a3a2ecf4a561c1423e203c2a52d1
Asim Abdalla <asimabdalla99@gmail.com>
2025-04-28 14:27:07 +0200

    update: revert batch size from 32 to 8 in watermark generation script

--- scripts/generate_with_watermark_translate.sh
+++ scripts/generate_with_watermark_translate.sh
@@ -12,7 +12,7 @@ MAPPING_DIR=$WORK_DIR/data/mapping
 TRANSFORM_MODEL=$WORK_DIR/data/model/transform_model_x-sbert.pth
 EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
 
-BATCH_SIZE=32
+BATCH_SIZE=8
 
 MODEL_NAMES=(
     "meta-llama/Llama-3.2-1B"
