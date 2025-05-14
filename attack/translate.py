8a130c7c80242e6b726934402eb147efc3bfa7e9
Asim Abdalla <asimabdalla99@gmail.com>
2025-05-14 18:42:49 +0200

    refactor: update translate.py to use OpenAI API

--- attack/translate.py
+++ attack/translate.py
@@ -4,111 +4,133 @@ import asyncio
 import requests
 import argparse
 from langcodes import Language
-from call_openai import (
-    CallOpenAI,
-)  # Updated to use OpenRouter-specific API call handling
-from dotenv import load_dotenv
-
-load_dotenv()
-
+from call_openai import CallOpenAI
 
 def valid_location():
-    res = requests.get("https://ipinfo.io", timeout=5).text
+    res = requests.get('https://ipinfo.io', timeout=5).text
     res = json.loads(res)
-    country = res.get("country", "")
+    country = res.get('country', '')
     print(json.dumps(res, indent=2))
     return country not in ["HK", "CN", "RU"]
-
 
 def read_jsonl(file_path):
     with open(file_path, "r") as f:
         return [json.loads(line) for line in f]
 
-
 def main(args):
-    assert os.getenv(
-        "OPENROUTER_API_KEY"
-    ), "Set the OPENROUTER_API_KEY environment variable"
-    # Load data
-    input_data = read_jsonl(args.input_file)
-
-    output_data = []
-    if os.path.exists(args.output_file):
-        output_data = read_jsonl(args.output_file)
-
-    print(
-        f"Translating {args.translate_part} from {args.src_lang} to {args.tgt_lang} using model {args.model}"
-    )
-    print(f"{len(input_data)} prompts found. {len(output_data)} translations found.")
-    if len(input_data) == len(output_data):
-        print("Translation already done. Skipping...")
-        return
+    # assert valid_location(), "Invalid location"
+    assert os.getenv("OPENAI_API_KEY"), "Set the OPENAI_API_KEY environment variable"
 
     def input_to_requests_func(input_file: str, output_file: str) -> list:
-        rqs = []
-        done_ids = []
+        """
+        Convert input file to a list of requests for OpenAI API.
 
+        Args:
+            input_file (str): The path to the input file.
+            output_file (str): The path to the output file.
+
+        Returns:
+            list: A list of requests for OpenAI API.
+
+        Note: Exclude the requests that have been done.
+        """
+
+        rqs = [] # list of requests
+        done_ids = [] # list of ids that have been done
+
+        # read the output file to get the ids that have been done
+        if os.path.isfile(output_file):
+            with open(output_file, "r") as f:
+                for line in f:
+                    done_ids.append(json.loads(line.strip())["id"])
+
+        # read the input file to form the requests
         with open(input_file, "r") as f:
             for i, line in enumerate(f):
                 if i in done_ids:
                     continue
                 prompt = json.loads(line.strip())["prompt"]
                 response = json.loads(line.strip())["response"]
-                # translate_part = json.loads(line.strip())[f"{args.translate_part}"]
                 rq = {
                     "model": args.model,
                     "messages": [
                         {
                             "role": "user",
-                            "content": f"Translate the following {Language.make(language=args.src_lang).display_name()} text to {Language.make(language=args.tgt_lang).display_name()}:\n\n{response}",
+                            "content": f"Translate the following {Language.make(language=args.src_lang).display_name()} text to {Language.make(language=args.tgt_lang).display_name()}:\n\n{response}"
                         }
                     ],
                     "temperature": args.temperature,
-                    "metadata": {
-                        "row_id": i,
-                        "prompt": prompt,
-                        "response": response,
-                        "translate_part": args.translate_part,
-                    },
+                    "metadata": {"row_id": i, "prompt": prompt} # store the id of the request as metadata
                 }
                 rqs.append(rq)
-        return rqs[:100]
+        return rqs
 
     def response_to_output_func(response: dict, output_file_path: str):
-        translation = response["response"]["choices"][0]["message"]["content"]
-        metadata = response["metadata"]
-        id = metadata["row_id"]
-        prompt = metadata["prompt"]
-        prompt_response = metadata["response"]
-        translation_part = metadata["translate_part"]
-        if translation_part == "prompt":
-            prompt = translation
-        elif translation_part == "response":
-            prompt_response = translation
+        """
+        Invoked after each succesful API call.
+        Convert OpenAI response to output and write it to output file.
 
+        Args:
+            response (dict): Dict of OpenAI response in the format of {"response": ..., "metadata": ...}.
+            output_file_path (str): The path to the output file.
+
+        Returns:
+            None
+        """
+
+        translation = response["response"]["choices"][0]["message"]["content"] # Extract the translation from the response
+        id = response["metadata"]["row_id"] # Extract the row ID from the metadata
+        prompt = response["metadata"]["prompt"]
+
+        # Write the translation to the output file as a json string. Temporarily, we use the output file as a jsonl file.
         json_string = json.dumps(
-            {"id": id, "prompt": prompt, "response": prompt_response},
-            ensure_ascii=False,
+            {
+                "id": id,
+                "prompt": prompt,
+                "response": translation
+            },
+            ensure_ascii=False
         )
         with open(output_file_path, "a") as f:
             f.write(json_string + "\n")
 
     def post_run_func(output_file_path: str):
+        """
+        Invoked after all API calls are done.
+        Organize the output file into the desired format
+
+        Args:
+            output_file_path (str): The path to the output file.
+
+        Returns:
+            None
+        """
+
+        # Read the output file and sort the translations by id
         results = []
-        with open(output_file_path, "r") as f:
+        with open(output_file_path, 'r') as f:
             for line in f:
                 results.append(json.loads(line.strip()))
-        results = sorted(results, key=lambda x: x["id"])
+        results = sorted(results, key=lambda x: x['id'])
 
+        # Write the translations to the output file
         with open(output_file_path, "w") as f:
             for r in results:
-                json_string = json.dumps(
-                    {"prompt": r["prompt"], "response": r["response"]},
-                    ensure_ascii=False,
-                )
+                json_string = json.dumps({"prompt": r["prompt"], "response": r["response"]}, ensure_ascii=False)
                 f.write(f"{json_string}\n")
 
     def is_all_done(input_file_path: str, output_file_path: str) -> bool:
+        """
+        Check if all the requests in the input file have been done.
+
+        Args:
+            input_file_path (str): The path to the input file.
+            output_file_path (str): The path to the output file.
+
+        Returns:
+            bool: True if all the requests have been done, False otherwise.
+        """
+
         if not os.path.isfile(output_file_path):
             return False
 
@@ -120,20 +142,23 @@ def main(args):
 
         return num_requests == num_done
 
-    openrouter_caller = CallOpenAI(
-        request_url="https://api.cerebras.ai/v1",
+    openai_caller = CallOpenAI(
+        request_url="https://api.openai.com/v1/chat/completions",
         api_key=os.getenv("OPENAI_API_KEY"),
         input_file_path=args.input_file,
         output_file_path=args.output_file,
         max_attempts=5,
+
+        # Set the functions for converting input to requests, converting response to output, running after all API calls are done, and checking if all requests have been done
         input_to_requests_func=input_to_requests_func,
         response_to_output_func=response_to_output_func,
         post_run_func=post_run_func,
-        is_all_done_func=is_all_done,
+        is_all_done_func=is_all_done
     )
 
-    asyncio.run(openrouter_caller.run())
-
+    asyncio.run(
+        openai_caller.run()
+    )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -143,8 +168,5 @@ if __name__ == "__main__":
     parser.add_argument("--temperature", type=float, default=0.0, help="Temperature")
     parser.add_argument("--src_lang", type=str, required=True, help="Source language")
     parser.add_argument("--tgt_lang", type=str, required=True, help="Target language")
-    parser.add_argument(
-        "--translate_part", type=str, required=True, help="Part to translate"
-    )
     args = parser.parse_args()
     main(args)
