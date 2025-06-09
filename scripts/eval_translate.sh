06805ff01830eb41c41dcc665b528ae8e0e5d46f
Asim Abdalla <asimabdalla99@gmail.com>
2025-06-09 23:24:31 +0200

    force update cluster id for token 11372

--- scripts/eval_translate.sh
+++ scripts/eval_translate.sh
@@ -3,37 +3,45 @@ set -u
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 WORK_DIR=$SCRIPT_DIR/..
-DATA_DIR=$WORK_DIR/data
 GEN_DIR=$WORK_DIR/gen
-ATTACK_DIR=$WORK_DIR/attack
 
-MAPPING_DIR=$WORK_DIR/data/mapping/xsir
-TRANSFORM_MODEL=$WORK_DIR/data/model/transform_model_x-sbert_10K.pth
-EMBEDDING_MODEL=paraphrase-multilingual-mpnet-base-v2
-
-BATCH_SIZE=32
 
 MODEL_NAMES=(
     "meta-llama/Llama-3.2-1B"
-    "bigscience/bloom-7b1"
-    "CohereForAI/aya-23-8B"
+    # "bigscience/bloom-7b1"
+    # "CohereForAI/aya-23-8B"
+    # "facebook/xglm-564M"
 )
 
 MODEL_ABBRS=(
     "llama-3.2-1B"
-    "bloom-7b1"
-    "aya-23-8B"
+    # "bloom-7b1"
+    # "aya-23-8B"
+    # "xglm-564M"
 )
 
 WATERMARK_METHODS=("xsir")
 TGT_LANGS=(
-    "it"
-    "es"
-    "pt"
-    "tr"
-    "sw"
-    "ar"
-    "am"
+    # High-resource languages
+    # "fr"
+    # "de"
+    # "it"
+    # "es"
+    # "pt"
+    # # Medium-resource languages
+    # "pl"
+    # "nl"
+    # "ru"
+    # "hi"
+    # "ko"
+    # "ja"
+    # # Low-resource languages
+    "bn"
+    # "fa"
+    # "vi"
+    # "iw" # Hebrew
+    # "uk"
+    # "ta"
     )
 SEEDS=(0 42 123)
 
@@ -50,8 +58,6 @@ for i in "${!MODEL_NAMES[@]}"; do
         for WATERMARK_METHOD in "${WATERMARK_METHODS[@]}"; do
 
             WATERMARK_DIR=$GEN_DIR/$MODEL_ABBR/${WATERMARK_METHOD}_seed${SEED}
-            MAPPING_FILE=$MAPPING_DIR/300_mapping_${MODEL_ABBR}_seed${SEED}.json
-            CLUSTERS_FILE=${MAPPING_FILE/_mapping_/_mapping_}_clusters.json
 
             echo "$MODEL_NAME $WATERMARK_METHOD (seed=$SEED) No-attack"
             python3 $WORK_DIR/eval_detection.py \
