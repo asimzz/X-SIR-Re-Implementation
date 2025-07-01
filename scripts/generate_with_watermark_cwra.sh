6ed1847559a4ea83b7ffc67b4132b7361a2c1bd4
Asim Abdalla <asimabdalla99@gmail.com>
2025-07-01 11:29:54 +0200

    update batch size to 32 for improved processing efficiency

--- scripts/generate_with_watermark_cwra.sh
+++ scripts/generate_with_watermark_cwra.sh
@@ -16,7 +16,7 @@ MAPPING_DIR="$DATA_DIR/mapping"
 TRANSFORM_MODEL="$DATA_DIR/model/transform_model_x-sbert.pth"
 EMBEDDING_MODEL="paraphrase-multilingual-mpnet-base-v2"
 
-BATCH_SIZE=8
+BATCH_SIZE=32
 
 # Model configurations
 MODEL_NAMES=(
